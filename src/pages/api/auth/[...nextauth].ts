42d486df8f36961a1911c3495a977e47ce67cdbf
Andson Lourenco <andson-lourenco@hotmail.com>
2022-04-08 17:54:54 -0300

    Saving user in database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,11 @@
+import {query as q} from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -13,6 +16,22 @@ export default NextAuth({
         }
       }
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn({user, account, profile}) {
+      const {email} = user
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            {data: {email}}
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
